d39091aa90f8fffed4d014e66bbb3ec8167b31a8
evhen <kokidko.evhen@gmail.com>
2021-10-01 14:55:28 +0300

    seeding db

--- public/index.php
+++ public/index.php
@@ -4,6 +4,56 @@ require_once '../vendor/autoload.php';
 require_once '../config/eloquent.php';
 require_once '../config/blade.php';
 
+//$categories =[];
+//
+//for ($i=1; $i <= 5; $i++) {
+//    $categories[] = [
+//        'title' => 'Category' . $i,
+//        'slug' => 'category-' . $i
+//    ];
+//}
+//
+//foreach ($categories as $category) {
+//    \Hillel\Models\Category::create($category);
+//}
+//
+//$posts = [];
+//$categories = \Hillel\Models\Category::all();
+//
+//for ($i=1; $i <= 10; $i++) {
+//    $posts[] = [
+//        'title' => 'Post' . $i,
+//        'slug' => 'post-' . $i,
+//        'body' => 'Body of post ' . $i,
+//        'category_id' => $categories->random()->id
+//    ];
+//}
+//
+//foreach ($posts as $post) {
+//    \Hillel\Models\Post::create($post);
+//}
+//
+//$tags =[];
+//
+//for ($i=1; $i <= 5; $i++) {
+//    $tags[] = [
+//        'title' => 'Tag' . $i,
+//        'slug' => 'tag-' . $i
+//    ];
+//}
+//
+//foreach ($tags as $tag) {
+//    \Hillel\Models\Tag::create($tag);
+//}
+//
+//$tags = \Hillel\Models\Tag::all();
+//$posts = \Hillel\Models\Post::all();
+//
+//foreach ($posts as $post) {
+//    $tagsId = $tags->pluck('id')->random(3);
+//    $post->tags()->sync($tagsId);
+//}
+
 $title = 'Абракадабра';
 
 /** @var $blade \Illuminate\View\Factory */
--- src/Models/Post.php
+++ src/Models/Post.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+        'category_id'
+        ];
     public function category()
     {
         return $this->belongsTo(Category::class);
